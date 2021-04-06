92eb22cd8509bf874b8c92313656804b906e4c23
Daniel Legorreta <d.legorreta.anguiano@gmail.com>
2021-04-06 17:30:47 -0500

    Create __init__.py

---
+++ notebook/pnud_textanalysis/similarity/__init__.py
@@ -0,0 +1,5 @@
+from .embeddings import Create_Embeddings
+from .similarity_sentence_embeddig import Similarity_SentencesEmbedding
+
+__all__=['Create_Embeddings',
+         'Similarity_SentencesEmbedding']
